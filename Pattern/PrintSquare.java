9fd55f6c58db8cb199e0f174967d89f0fbe2a687
SHIVAM <93383164+Shivam-090@users.noreply.github.com>
2022-10-08 14:43:35 +0530

    Pattern 2

--- Pattern/PrintSquare.java
+++ Pattern/PrintSquare.java
@@ -1,12 +1,13 @@
-package Pattern_Problems;
+package Pattern;
+
 import java.util.*;
-public class PrintTri{
+public class PrintSquare{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the Triangle: ");
+        System.out.println("Enter the size of the square: ");
         int size = sc.nextInt();
-        for (int i = 1; i <= size; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print("* ");
             }
             System.out.println();
