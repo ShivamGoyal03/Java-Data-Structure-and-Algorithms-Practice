acd571990e774d15593157e2351a20be2d4020e4
SHIVAM <shivam21csu090@ncuindia.edu>
2023-02-23 23:31:50 +0530

    Arrays And ArrayList

---
+++ ArraysAndArrayList/Swapping.java
@@ -0,0 +1,26 @@
+package ArraysAndArrayList;
+import java.util.*;
+
+import javax.swing.text.html.StyleSheet;
+public class Swapping {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        swap(arr, 1, 2);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);  
+            start++; end--;
+        }
+    }
+}
